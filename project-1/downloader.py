34ef98ec7add096478671ea9db0415401dd2801a
Mattia <82611659+Xhst@users.noreply.github.com>
2024-10-07 14:23:31 +0200

    feat: downloader
    Co-Authored-By: aledigirm3 <100938136+aledigirm3@users.noreply.github.com>
    Co-Authored-By: Carlo Proserpio <115183769+Prox747@users.noreply.github.com>

---
+++ project-1/downloader.py
@@ -0,0 +1,36 @@
+import requests
+import paths
+from lxml import html
+
+TOPIC = "cs.AI"
+NUMBER_OF_PAPERS = 750
+ALLOW_DOWNLOAD = False
+
+URL = f"https://arxiv.org/list/{TOPIC}/recent?skip=0&show={NUMBER_OF_PAPERS}"
+
+response = requests.get(URL)
+
+print("GET Response Status Code:", response.status_code)
+if (response.status_code != 200):
+    print("GET Request Failed")
+    exit()
+
+arxiv = html.fromstring(response.content)
+
+paper_anchor_tags = arxiv.xpath('//*[@id="articles"]//dt//a[text()="html"]')
+print(f"Found {len(paper_anchor_tags)} papers (with html format) to download")
+
+for anchor in paper_anchor_tags:
+    url: str = anchor.xpath("@href")[0]
+    id: str = anchor.xpath("@id")[0]
+
+    # remove the html- prefix
+    paper_id = id.replace("html-", "")
+
+    response = requests.get(url)
+    html_content = str(response.text)
+
+    if ALLOW_DOWNLOAD: 
+        with open(f"{paths.HTML_FOLDER}{paper_id}.html", "w", encoding="utf-8") as file:
+            file.write(html_content)
+            
