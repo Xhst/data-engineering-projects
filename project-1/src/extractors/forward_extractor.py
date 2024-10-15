87a8ecefe524010effe6fbdd75b7241b523a6651
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-10-15 16:34:57 +0200

    fix: references

--- project-1/src/extractors/forward_extractor.py
+++ project-1/src/extractors/forward_extractor.py
@@ -2,7 +2,7 @@ from lxml import html
 from json_schema import TableData
 
 
-def extract_references(table_num: str, table_id : str, paper : html.HtmlElement) -> list[str]:
+def extract_references(table_num: str, table_id: str, paper : html.HtmlElement) -> list[str]:
     """
     Extracts paragraphs text where references to the table are found.
 
@@ -10,14 +10,18 @@ def extract_references(table_num: str, table_id : str, paper : html.HtmlElement)
 
     Parameters:
         table_id (str): The table ID we are searching the references for.
+        table_num (str): The table number written like this: "Table 1"
         paper (html.HtmlElement): The paper HTML element from which to extract the references and section titles.
 
     Returns:
-        list[str]: A list of section titles associated with the references to the table.
+        list[str]: A list of paragraphs' text associated with the references to the table.
     """
     refs_text: list[str] = []
     
-    paragraphs_ref = paper.xpath(f'//a[contains(@href, {table_id})]/..')
+    # get the first two parts of the table id (e.g. #S1.T1.1.2 -> #S1.T1) because it's the id usally used in references
+    id = ".".join(table_id.split(".")[:2])
+    
+    paragraphs_ref = paper.xpath(f'//a[contains(@href, "{id}")]/..')
     
     # If there are no link references, we try to find the ref by table number text (es. "Table 1")
     if paragraphs_ref == []:
@@ -36,14 +40,26 @@ def extract_references(table_num: str, table_id : str, paper : html.HtmlElement)
     return refs_text
 
 
-def extract_caption(table: html.HtmlElement, table_id: str) -> str:
+def extract_caption(table_container: html.HtmlElement, table_id: str) -> str:
+    """
+    Extracts captions.
+
+    It searches for captions inside the table container. It tries strictier rules where necessary.
+
+    Parameters:
+        table_id: The table ID we are searching the caption for.
+        table_container (html.HtmlElement): The table container HTML element from which to extract the caption.
+
+    Returns:
+        list[str]: A list of section titles associated with the references to the table.
+    """
     # Extracting captions ("Table X: " + "caption")
-    caption_fragments = table.xpath('.//*[contains(@class, "ltx_caption")]//text()')
+    caption_fragments = table_container.xpath('.//*[contains(@class, "ltx_caption")]//text()')
     # If we did not found any caption, we try a stricter rules
     if caption_fragments == []:
-        caption_fragments = table.xpath(f'./p[contains(@id, "{table_id}.")]//text()')
+        caption_fragments = table_container.xpath(f'./p[contains(@id, "{table_id}.")]//text()')
     if caption_fragments == []:
-        caption_fragments = table.xpath(f'./span[contains(@id, "{table_id}.")]//text()')
+        caption_fragments = table_container.xpath(f'./span[contains(@id, "{table_id}.")]//text()')
 
     caption = "".join(caption_fragments)
     
@@ -96,55 +112,46 @@ def extract_paper_data(paper: html.HtmlElement) -> dict[str, TableData]:
     # Most tables are contained within a figure element with a class of "ltx_table" and id containing ".T", 
     # while in rare cases they are contained within a div element with a class of "ltx_minipage".
     # In that case, we select only those divs that contain a table element.
-    table_containers = paper.xpath('//figure[contains(@id, ".T") and contains(@class, "ltx_table")] | ' + 
-                                   '//table/ancestor::div[contains(@id, ".") and contains(@class, "ltx_minipage")]')
-
-    for table in table_containers:
-        if table == None or table == [] or not(table.getchildren()):
+    # TODO: confront with others
+    table_containers = paper.xpath('//figure[contains(@id, ".T")] | ' + 
+                                   '//table/ancestor::div[contains(@id, ".") and contains(@class, "ltx_minipage") and not(ancestor::figure)]')    
+    
+    for table_container in table_containers:
+        if table_container == None or table_container == [] or not(table_container.getchildren()):
             continue
         
         # we exclude equations and figures (id = ".E" or ".F")
-        table_tags = table.xpath('.//table[contains(@id, ".T")]')
+        table_tags = table_container.xpath('.//table[contains(@id, ".T")]')
+        
+        if table_tags == []:
+            continue
     
-        table_id = table.get('id')
+        table_id = table_container.get('id')
         
         # Extracting caption
-        caption = extract_caption(table, table_id)
+        caption = extract_caption(table_container, table_id)
 
         # Extracting footnotes
-        footnotes = extract_footnotes(table)
+        footnotes = extract_footnotes(table_container)
         
         # Extracting references
         # We need this to search for some references, we take the first part of the caption ("Table X:")
         table_num = caption.split(":")[0]
         paragraphs_refs = extract_references(table_num, table_id, paper)
 
-        
-        # If there are no tables, we may have found a table caption related to a <table> tag
-        # that is not a direct parent of the table element, or maybe is outside of the figure element. 
-        # In this case, we need to add the caption to the
-        # exisitng table JSON and add footnotes that may lay inside the caption
-        if table_tags == []:
-            if table_id in article_json:
-                article_json[table_id].caption = caption
-                existing_footnotes: set[str] = set(article_json[table_id].footnotes)
-                existing_footnotes.update(footnotes)
-                article_json[table_id].footnotes = list(existing_footnotes)
-        
-        else:
-            # If there are tables, we just insert it into the JSON
-            table_tag_str = ""
-            for table_tag in table_tags:
-                table_tag_str = table_tag_str + html.tostring(table_tag).decode('utf-8')
+        # We then insert it into the json
+        table_tag_str = ""
+        for table_tag in table_tags:
+            table_tag_str = table_tag_str + html.tostring(table_tag).decode('utf-8')
 
-            # populate TableData
-            table_json = TableData()
-            table_json.caption = caption
-            table_json.table = table_tag_str
-            table_json.footnotes = footnotes
-            table_json.references = paragraphs_refs
-            
-            # Store the table JSON with its id
-            article_json[table_id] = table_json
+        # populate TableData
+        table_json = TableData()
+        table_json.caption = caption
+        table_json.table = table_tag_str
+        table_json.footnotes = footnotes
+        table_json.references = paragraphs_refs
         
+        # Store the table JSON with its id
+        article_json[table_id] = table_json
+    
     return article_json
