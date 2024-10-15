fbcdde0b2bfb77dde754637344a3d2cfa52b86ab
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-10-15 14:30:17 +0200

    refactor: delete v4 added v2

--- project-1/extractorV2.py
+++ project-1/extractorV2.py
@@ -1,64 +1,60 @@
 import json
 import paths
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from dataclasses import dataclass, field
 from tqdm import tqdm
 from lxml import html
 
-# Set this to True if you want to check if the HTML files contain tables
-CHECK_TABLE_PRESENCE = False
+@dataclass
+class TableData:
+    caption: str = ""
+    table: str = ""
+    footnotes: list[str] = field(default_factory=list)
+    references: list[str] = field(default_factory=list)
 
-def extract_references(table_id : str, paper : html.HtmlElement) -> list[str]:
-    """
-    Extracts section titles where references to the table are found.
 
-    It searches for all the references linked to a specific table ID, then finds the first ancestor
-    section of that reference, and extracts the section title fragments contained within that section.
+def extract_paper_title(paper: html.HtmlElement) -> str:
+    title = paper.xpath('//title/text()')[0]
+    return title
 
-    Parameters:
-        table_id (str): The table ID we are searching the references for.
-        paper (html.HtmlElement): The paper HTML element from which to extract the references and section titles.
 
-    Returns:
-        list[str]: A list of section titles associated with the references to the table.
-    """
-    paragraphs_ref = paper.xpath(f'//a[contains(@href, "{table_id}") and contains(@class, "ltx_ref")]/ancestor::section[1]/*[contains(@class, "ltx_title")]')
+def extract_table_caption(table: html.HtmlElement, usedCaptions: set[str]) -> tuple[str, str]:
     
-    ref_titles: list[str] = []
-    for par in paragraphs_ref:
-        # Build each section title from the fragments
-        sec_title = ""
-        title_fragments = par.xpath('.//text()')
-        for fragment in title_fragments:
-            if fragment == "" or fragment == "\n":
-                continue
-            sec_title += fragment
+    # get the first valid caption found in the following xpaths
+    xpaths = [
+        './/preceding-sibling::*[contains(@class, "ltx_caption")][1]//text()',
+        './/following-sibling::*[contains(@class, "ltx_caption")][1]//text()',
+        './/preceding::*[contains(@class, "ltx_caption")][1]//text()',
+        './/following::*[contains(@class, "ltx_caption")][1]//text()'
+    ]
+
+    for xpath in xpaths:
+        caption_parts = table.xpath(xpath)
         
-        ref_titles.append(sec_title)
-    
-    return ref_titles
+        if caption_parts:
+            caption = ''.join(caption_parts)
 
-        
-def extract_footnotes(table : html.HtmlElement) -> list[str]:
-    """
-    Extracts the footnotes from the table by combining the non-numeric text segments 
-    of each footnote, ignoring specific nested elements.
+            if caption not in usedCaptions:
+                return caption, caption_parts[0]
     
-    The structure of every footnote seems to be:
-        footnote id = "footnote1" (the most outer part)\n
-        |__> footnote id = "footnote1.1" (an hidden html element that appears 
-                when mouse hovering -> which we are not interested in)\n
-            |__> several text fragments componing the footnote (we take these directly from the first element)
+    return '', ''
 
-    Of these fragments, the first ones are usually the footnote number repeated, so we can ignore them
-    Then, we combine the rest
+
+def extract_table(table: html.HtmlElement) -> str:
+    return html.tostring(table).decode('utf-8')
+
+
+def extract_table_footnotes(table: html.HtmlElement, table_id: str) -> list[str]:
+    id = ".".join(table_id.split(".")[:2])
+    table_container = table.xpath(f'.//ancestor::figure[@id="{id}"][1]')
+
+    if not table_container:
+        return []
     
-    Parameters:
-        table (html.HtmlElement): The table element from which to extract the footnotes.
+    table_container = table_container[0]
 
-    Returns:
-        list[str]: A list of strings where each string is a table footnote's full text.
-    """
-    footnotes_xpath = table.xpath('.//*[contains(@id, "footnote") and not(contains(@id, "."))]')
+    footnotes_xpath = table_container.xpath('.//*[contains(@id, "footnote") and not(contains(@id, "."))]')
     footnotes: list[str] = []
     
     for footnote in footnotes_xpath:
@@ -76,99 +72,112 @@ def extract_footnotes(table : html.HtmlElement) -> list[str]:
     return footnotes
 
 
-def extract_caption(table : html.HtmlElement) -> str:
-    # Extracting captions ("Table X: " + "caption")
+def extract_table_references(paper: html.HtmlElement, table_id: str, tableDenomination: str) -> list[str]:
+    result = []
 
-    # First we control the siblings
-    caption_fragments = table.xpath('/sibling:://*[contains(@class, "ltx_caption")]//text()')
-    # If we did not found any caption, we try a stricter rules
-    if caption_fragments == []:
-        caption_fragments = table.xpath(f'./p[contains(@id, "{table_id}.")]//text()')
-    if caption_fragments == []:
-        caption_fragments = table.xpath(f'./span[contains(@id, "{table_id}.")]//text()')
+    # get the first two parts of the table id (e.g. #S1.T1.1.2 -> #S1.T1) because it's the id usally used in references
+    id = ".".join(table_id.split(".")[:2])
+
+    a_refs = paper.xpath(f'//a[contains(@href, "{id}")]/..')
+
+    # first try to find references by looking for <a> tags
+    if a_refs:
+        for a_ref in a_refs:
+            text_ref_content = a_ref.text_content().strip() if hasattr(a_ref, 'text_content') else a_ref.strip()
+            result.append(text_ref_content)
+    
+    # if no references are found, try to find them by looking for text references
+    elif tableDenomination != "":
+        
+        # find all text references that contain the table denomination followed by a space, a dot,
+        # a colon or a comma. It's necessary to avoid selecting tables with a "greater" denomination.
+        # (e.g. "Table 1" and "Table 11", or "Table II" and "Table III") 
+        text_refs = paper.xpath(f'//*[contains(normalize-space(text()), "{tableDenomination} ") or '
+                                f'contains(normalize-space(text()), "{tableDenomination}.") or '
+                                f'contains(normalize-space(text()), "{tableDenomination}:") or '
+                                f'contains(normalize-space(text()), "{tableDenomination},")]')
+
+        for text_ref in text_refs: 
+            text_ref_content = text_ref.text_content().strip() if hasattr(text_ref, 'text_content') else text_ref.strip()
+    
+            # if the text is exactly the table denomination followed by a colon, it's not a reference, but the caption
+            if text_ref_content != tableDenomination + ":":
+                result.append(text_ref_content)
+
+    return result
 
 
-papers_no_tables: list[str] = []
+def extract_paper_data(paper: html.HtmlElement, filename: str) -> dict[str, TableData]:
+
+    tablesData: dict[str, TableData] = {}
+    usedCaptions: set[str] = set()
+
+    tables: list[html.HtmlElement] = paper.xpath('//table[contains(@class, "ltx_tabular") and not(ancestor::table)]')
+
+    for table in tables:
+        tableData = TableData()
+
+        table_id = table.get('id')
+        
+        tableData.caption, tableDenomination = extract_table_caption(table, usedCaptions)
+        usedCaptions.add(tableData.caption)
+
+        tableDenomination = tableDenomination.replace(":", "").strip()
+
+        tableData.table = extract_table(table)
+        tableData.footnotes = extract_table_footnotes(table, table_id)
+        tableData.references = extract_table_references(paper, table_id, tableDenomination)
+
+        tablesData[table_id] = tableData
+
+    return tablesData
+    
+
+def process_file(filename, source_folder, extract_folder):
+    with open(f"{source_folder}/{filename}", "r", encoding="utf-8") as htmlFile:
+        file_content = htmlFile.read().encode('utf-8')
+        paper = html.fromstring(file_content, parser=html.HTMLParser())
+        
+        filename = filename.replace(".html", "")
+
+        paperData = extract_paper_data(paper, filename)
+
+        with open(f"{extract_folder}/{filename}.json", "w", encoding="utf-8") as jsonFile:
+            json.dump(paperData, jsonFile, default=lambda o: o.__dict__, indent=4)
+
 
 if __name__ == "__main__":
-    filenames = os.listdir(paths.HTML_FOLDER)
+    html_folder = paths.HTML_FOLDER
+    json_folder = paths.JSON_FOLDER
 
-    # make sure the JSON folder exists
-    if not os.path.exists(paths.JSON_FOLDER):
-        os.makedirs(paths.JSON_FOLDER)
+    folders_to_extract = ['record_linkage', 'synthetic_data', 'ia']
 
-    for filename in tqdm(filenames, desc="Processing HTML files", unit=" file", colour="green", disable=True):
-        article_json : dict[str, dict] = {}
+    for folder in folders_to_extract:
+        if not os.path.exists(f"{json_folder}/{folder}"):
+            os.makedirs(f"{json_folder}/{folder}")
+
+    filenames = os.listdir(html_folder)
+
+    max_workers = 8
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for folder in folders_to_extract:
+
+            extract_folder = f"{json_folder}/{folder}"
+            source_folder = f"{html_folder}/{folder}"
+
+            filenames = os.listdir(f"{source_folder}")
+
+            futures = {
+                executor.submit(process_file, filename, source_folder, extract_folder): filename for filename in filenames
+            }
+
+            for future in tqdm(as_completed(futures), desc=f"Processing {source_folder}", unit=" file", colour="green", total=len(futures)):
+                try:
+                    future.result() 
+                except Exception as e:
+                    print(f"Error processing file {futures[future]}: {e}")
+
+
+
         
-        with open(f"{paths.HTML_FOLDER}/{filename}", "r", encoding="utf-8") as file:
-            file_content = file.read()
-            paper = html.fromstring(file_content)
-
-            if CHECK_TABLE_PRESENCE:
-                all_table_tags: list[html.HtmlElement] = paper.xpath('//table[not(contains(@id, ".E") or contains(@id, ".F"))]')
-                if all_table_tags == []:
-                    papers_no_tables.append(filename.replace(".html", ""))
-                        
-            # Extracting tables, here "tables" are intended as table tags
-            
-            # Most tables are contained within a figure element with a class of "ltx_table" and id containing ".T", 
-            # while in rare cases they are contained within a div element with a class of "ltx_minipage".
-            # In that case, we select only those divs that contain a table element.
-            tables = paper.xpath('//figure[contains(@id, ".T") and contains(@class, "ltx_table")]/descendant::table | ' + 
-                                 '//div[contains(@id, ".") and contains(@class, "ltx_minipage")]/descendant::table')
-        
-            for table in tables:
-                if table == None or table == [] or not(table.getchildren()):
-                    continue
-
-                # Skip tables that are equations or figures or other
-                if not(".T" in table.xpath('@id')[0]):
-                    continue
-                
-                # we extract the table id and format it like this: T1, T2, T3, ...
-                table_id = ".".join(table.xpath('@id')[0].split(".")[1:])
-
-                if filename == "2410.01064.html":  
-                    print(table_id)
-                
-                caption = extract_caption(table)
-
-                # Extracting footnotes
-                footnotes = extract_footnotes(table)
-                
-                # Extracting references
-                paragraphs_refs = extract_references(table_id, paper)
-                
-                # Finally, some ids are missing the "T" prefix, so we add it
-                if not("T" in table_id):
-                    table_id = "T" + table_id
-
-                # Check to see if tables are inside the table xpath
-                table_str = ""
-                if(table.xpath('.//table') != []):
-                    # If there are tables, we insert it into the JSON
-                    table_str = html.tostring(table.xpath('.//table')[0]).decode('utf-8')
-
-                    table_json = {
-                        "caption": caption,
-                        "table": table_str,
-                        "footnotes": footnotes,
-                        "references": paragraphs_refs
-                    }
-                    
-                    # Store the table JSON with its id
-                    article_json[table_id] = table_json
-                else:
-                    # If there are no tables, we may have found a table caption related to a <table> tag
-                    # that is not a direct child of the figure element. In this case, we need to add the caption to the
-                    # exisitng table JSON and add footnotes that may lay inside the caption
-                    if table_id in article_json:
-                        article_json[table_id]["caption"] = caption
-                        existing_footnotes: set[str] = set(article_json[table_id]["footnotes"])
-                        existing_footnotes.update(footnotes)
-                        article_json[table_id]["footnotes"] = list(existing_footnotes)
-                
-        # Change extension to json and save to file
-        filename = filename.replace(".html", ".json")
-        with open(f"{paths.JSON_FOLDER}/{filename}", "w", encoding="utf-8") as json_file:
-            json.dump(article_json, json_file, indent=4)
