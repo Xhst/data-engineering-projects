1a87e99251ed8e3edbf3dc4aff65829bffc6ada2
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-03 15:51:26 +0100

    feat: web config

--- project-2/server/src/main/java/it/uniroma3/idd/project_2/SearchController.java
+++ project-2/server/src/main/java/it/uniroma3/idd/project_2/SearchController.java
@@ -1,7 +1,10 @@
 package it.uniroma3.idd.project_2;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class SearchController {
 
     private final SearchService searchService;
+
+    @GetMapping
+    public ResponseEntity<?> search(@RequestParam String query) {
+        return ResponseEntity.ok("Test " + query);
+    }
 }
