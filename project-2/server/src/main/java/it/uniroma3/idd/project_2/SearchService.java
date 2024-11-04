9c74cee627113d67f4ea63b440f434ee81f8b8d1
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-04 15:39:45 +0100

    feat: index documents

--- project-2/server/src/main/java/it/uniroma3/idd/project_2/SearchService.java
+++ project-2/server/src/main/java/it/uniroma3/idd/project_2/SearchService.java
@@ -2,24 +2,58 @@ package it.uniroma3.idd.project_2;
 
 import org.apache.lucene.analysis.Analyzer;
 
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.*;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Map;
 
 @Service
 public class SearchService {
 
     private final Directory index;
-    private final Map<String, Analyzer> perFieldAnalyzer;
+    private final IndexWriterConfig config;
     private final Path sourcesPath;
 
     public SearchService(SearchConfig searchConfig) throws IOException {
         this.index = searchConfig.indexDirectory();
-        this.perFieldAnalyzer = searchConfig.perFieldAnalyzer();
+        this.config = searchConfig.indexWriterConfig();
         this.sourcesPath = searchConfig.getSourcesPath();
+
+        this.indexDocuments();
+    }
+
+    private void indexDocuments() throws IOException {
+        IndexWriter writer = new IndexWriter(index, config);
+        writer.deleteAll();
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(sourcesPath)) {
+            for (Path path : stream) {
+                if (Files.isDirectory(path)) continue;
+
+                DocumentParser parser = new DocumentParser(path);
+                Document document = new Document();
+
+                document.add(new TextField("title", parser.getTitle(), Field.Store.YES));
+                document.add(new TextField("authors", parser.getAuthors(), Field.Store.YES));
+                document.add(new TextField("keywords", parser.getKeywords(), Field.Store.YES));
+                document.add(new TextField("abstract", parser.getAbstract(), Field.Store.YES));
+                document.add(new TextField("content", parser.getContent(), Field.Store.NO));
+
+                writer.addDocument(document);
+            }
+        }
+
+        writer.commit();
+        writer.close();
     }
 
 }
