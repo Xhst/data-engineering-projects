210032f7db50b46e70463d87a4efb359ec1186e2
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-05 10:02:12 +0100

    feat: build query

--- project-2/client/src/application.ts
+++ project-2/client/src/application.ts
@@ -11,20 +11,18 @@ function addFilter() {
 
     let filterInputTemplate = `
         <div id="as-filter-container-${advancedSearchQueriesNumber}" class="input-group mb-3">
-            <select class="form-select" id="as-operator-${advancedSearchQueriesNumber}"
-                style="max-width: 90px">
+            <select class="form-select" id="as-operator-${advancedSearchQueriesNumber}" style="max-width: 90px">
                 <option value="and" selected>AND</option>
                 <option value="or">OR</option>
                 <option value="not">NOT</option>
             </select>
-            <input type="text" class="form-control" aria-label="Text input with dropdown button">
-            <select class="form-select" id="as-filter-${advancedSearchQueriesNumber}"
-                style="max-width: 120px">
-                <option value="1" selected>All</option>
-                <option value="2">Title</option>
-                <option value="3">Abstract</option>
-                <option value="3">Keywords</option>
-                <option value="3">Authors</option>
+            <input id="as-query-${advancedSearchQueriesNumber}" type="text" class="form-control">
+            <select id="as-filter-${advancedSearchQueriesNumber}" class="form-select" style="max-width: 120px">
+                <option value="all" selected>All</option>
+                <option value="title">Title</option>
+                <option value="abstract">Abstract</option>
+                <option value="keywords">Keywords</option>
+                <option value="authors">Authors</option>
             </select>
             <button class="btn btn-outline-danger" id="as-remove-filter-${advancedSearchQueriesNumber}"><i class="bi bi-x"></i></button>
         </div>
@@ -37,11 +35,33 @@ function addFilter() {
         document.getElementById(`as-filter-container-${advancedSearchQueriesNumber}`).remove();
         filtersIds.delete(advancedSearchQueriesNumber);
     });
-
-    console.log(filtersIds);
 }
 
 document.getElementById("as-add-filter-button").addEventListener("click", () => { addFilter(); });
+
+function buildAdvancedQuery() {
+    let text = (document.getElementById("as-query") as HTMLInputElement).value;
+    let filter = (document.getElementById("as-filter") as HTMLSelectElement).value;
+    let operator = ""
+
+    filter = filter === "all" ? "" : filter + ":";
+
+    let query = `${filter}${text}`
+
+    filtersIds.forEach((id) => {
+        operator = (document.getElementById(`as-operator-${id}`) as HTMLSelectElement).value;
+        filter = (document.getElementById(`as-filter-${id}`) as HTMLSelectElement).value;
+        text = (document.getElementById(`as-query-${id}`) as HTMLInputElement).value;
+
+        filter = filter === "all" ? "" : filter + ":";
+
+        if (text === "") return;
+
+        query += ` ${operator} ${filter}${text}`;
+    });
+
+    return query;
+}
 /*
 axios.get("http://localhost:3000/api/search?query=AAA").then((response) => {
     console.log(response.data);
