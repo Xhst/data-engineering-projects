618c52a9b16bc31fa82e507b75655b35dfa593cc
aledigirm3 <alessandro.digirolamo.1937@gmail.com>
2024-11-06 17:59:33 +0100

    p2-server: all analyzer

--- project-2/server/src/main/java/it/uniroma3/idd/project_2/SearchConfig.java
+++ project-2/server/src/main/java/it/uniroma3/idd/project_2/SearchConfig.java
@@ -1,8 +1,13 @@
 package it.uniroma3.idd.project_2;
 
-import org.apache.lucene.analysis.core.WhitespaceTokenizerFactory;
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.core.StopFilterFactory;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
-import org.apache.lucene.analysis.pattern.PatternReplaceFilterFactory;
+import org.apache.lucene.analysis.en.PorterStemFilterFactory;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
+import org.apache.lucene.analysis.miscellaneous.RemoveDuplicatesTokenFilterFactory;
+import org.apache.lucene.analysis.miscellaneous.TrimFilterFactory;
+import org.apache.lucene.analysis.synonym.SynonymGraphFilterFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +17,10 @@ import org.apache.lucene.codecs.simpletext.SimpleTextCodec;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -28,6 +35,9 @@ public class SearchConfig {
 
     @Value("${search.sources.path}")
     private String sourcesPath;
+
+    @Value("${search.synonyms.path}")
+    private String synonymsFilePath;
 
     @Bean
     public Directory indexDirectory() throws IOException {
@@ -54,21 +64,72 @@ public class SearchConfig {
         return config;
     }
 
+
     public Map<String, Analyzer> getPerFieldAnalyzers(){
 
        try {
            CustomAnalyzer.Builder titleAnalyzerBuilder = CustomAnalyzer.builder()
-                   .withTokenizer(WhitespaceTokenizerFactory.class)  //Filter for remove whitespace
-                   .addTokenFilter(PatternReplaceFilterFactory.class); //Filter for remove punctuation
+                   // Filter that remove whitespace and punctuation
+                   .withTokenizer(StandardTokenizerFactory.class)
+                   // Converts all tokens to lowercase
+                   .addTokenFilter(LowerCaseFilterFactory.class)
+                   // Removes whitespace at the beginning and end of tokens
+                   .addTokenFilter(TrimFilterFactory.class);
+
+           CustomAnalyzer.Builder authorsAnalyzerBuilder = CustomAnalyzer.builder()
+                   .withTokenizer(StandardTokenizerFactory.class)
+                   .addTokenFilter(LowerCaseFilterFactory.class)
+                   .addTokenFilter(TrimFilterFactory.class);
+
+           CustomAnalyzer.Builder keywordAnalyzerBuilder = CustomAnalyzer.builder()
+                   .withTokenizer(StandardTokenizerFactory.class)
+                   .addTokenFilter(LowerCaseFilterFactory.class)
+                   .addTokenFilter(TrimFilterFactory.class);
+
+           CustomAnalyzer.Builder abstractAnalyzerBuilder = CustomAnalyzer.builder()
+                   .withTokenizer(StandardTokenizerFactory.class)
+                   .addTokenFilter(LowerCaseFilterFactory.class)
+                   .addTokenFilter(TrimFilterFactory.class)
+                   // Apply a stemming algorithm to reduce words to their root
+                   .addTokenFilter(PorterStemFilterFactory.class)
+                   // Remove stop-words
+                   .addTokenFilter(StopFilterFactory.class)
+                   // Removes consecutive duplicate tokens
+                   .addTokenFilter(RemoveDuplicatesTokenFilterFactory.class)
+                   // Converts accented characters to plain ASCII characters
+                   .addTokenFilter(ASCIIFoldingFilterFactory.class);
+                    // Handles synonyms, allowing you to map similar or alternative words to a common token
+//                   .addTokenFilter(SynonymGraphFilterFactory.class,
+//                           "synonyms", String.valueOf(Paths.get(synonymsFilePath)),  // Pass the path to the synonyms file
+//                           "expand", "true");  // Expand synonyms
+
+           CustomAnalyzer.Builder contentAnalyzerBuilder = CustomAnalyzer.builder()
+                   .withTokenizer(StandardTokenizerFactory.class)
+                   .addTokenFilter(LowerCaseFilterFactory.class)
+                   .addTokenFilter(TrimFilterFactory.class)
+                   // Apply a stemming algorithm to reduce words to their root
+                   .addTokenFilter(PorterStemFilterFactory.class)
+                   // Remove stop-words
+                   .addTokenFilter(StopFilterFactory.class)
+                   // Removes consecutive duplicate tokens
+                   .addTokenFilter(RemoveDuplicatesTokenFilterFactory.class)
+                   // Converts accented characters to plain ASCII characters
+                   .addTokenFilter(ASCIIFoldingFilterFactory.class);
+                   // Handles synonyms, allowing you to map similar or alternative words to a common token
+//                   .addTokenFilter(SynonymGraphFilterFactory.class,
+//                           "synonyms", String.valueOf(Paths.get(synonymsFilePath)),  // Pass the path to the synonyms file
+//                           "expand", "true");  // Expand synonyms
 
 
         return Map.of(
                 "title", titleAnalyzerBuilder.build(),
-                "authors", new StandardAnalyzer(),
-                "keywords", new StandardAnalyzer(),
-                "abstract", new StandardAnalyzer(),
-                "content", new StandardAnalyzer()
+                "authors", authorsAnalyzerBuilder.build(),
+                "keywords", keywordAnalyzerBuilder.build(),
+                "abstract", abstractAnalyzerBuilder.build(),
+                "content", contentAnalyzerBuilder.build()
         );
+
+
        } catch (IOException e) {
            throw new RuntimeException(e);
        }
