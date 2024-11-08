790838d0c8cf40b6a3dab1c65a839c0886908067
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-08 17:21:17 +0100

    style: removed unused imports

--- project-2/server/src/main/java/it/uniroma3/idd/project_2/Indexer.java
+++ project-2/server/src/main/java/it/uniroma3/idd/project_2/Indexer.java
@@ -7,8 +7,6 @@ import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
