ff0e5bdf7bd56285f17b7c9819deec7d1ff4c66f
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-08 14:04:52 +0100

    feat: query time

--- project-2/server/src/main/java/it/uniroma3/idd/project_2/SearchService.java
+++ project-2/server/src/main/java/it/uniroma3/idd/project_2/SearchService.java
@@ -1,5 +1,6 @@
 package it.uniroma3.idd.project_2;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -20,94 +21,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Slf4j
+@AllArgsConstructor
 public class SearchService {
 
-    private final Path sourcePath;
-    private final IndexWriter indexWriter;
     private final IndexSearcher indexSearcher;
     private final MultiFieldQueryParser queryParser;
 
-    public SearchService(
-            Path sourcePath,
-            IndexWriter indexWriter,
-            IndexSearcher indexSearcher,
-            MultiFieldQueryParser queryParser) throws IOException {
-        this.sourcePath = sourcePath;
-        this.indexWriter = indexWriter;
-        this.indexSearcher = indexSearcher;
-        this.queryParser = queryParser;
-
-        this.indexDocuments();
-    }
-
-    private static void printLoadingBar(int indexedFiles, int totalFiles) {
-        int barLength = 100; // Total length of the loading bar
-        int progress = (int) ((double) indexedFiles / totalFiles * barLength);
-
-        StringBuilder bar = new StringBuilder();
-        bar.append("\r Indexing - [");
-        for (int i = 0; i < barLength; i++) {
-            if (i < progress) {
-                bar.append("#");
-            } else {
-                bar.append("-");
-            }
-        }
-        bar.append("] ");
-        bar.append(indexedFiles).append("/").append(totalFiles);
-        bar.append(" files.");
-
-        // Print the progress bar
-        System.out.print(bar);
-
-        // Print a new line when indexing is complete
-        if (indexedFiles == totalFiles) {
-            System.out.println();
-        }
-    }
-
-    private void indexDocuments() throws IOException {
-        indexWriter.deleteAll();
-
-        log.info("Start Indexing");
-
+    public SearchDto search(String queryString) throws IOException, ParseException {
         long startTime = System.currentTimeMillis();
-
-        int totalFiles = 9235; //9372;
-        int indexedFiles = 0;
-
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(sourcePath)) {
-            for (Path path : stream) {
-                if (Files.isDirectory(path)) continue;
-
-                DocumentParser parser = new DocumentParser(path);
-                Document document = new Document();
-
-                document.add(new TextField("filename", parser.getFileName(), Field.Store.YES));
-                document.add(new TextField("title", parser.getTitle(), Field.Store.YES));
-                document.add(new TextField("authors", parser.getAuthors(), Field.Store.YES));
-                document.add(new TextField("keywords", parser.getKeywords(), Field.Store.YES));
-                document.add(new TextField("abstract", parser.getAbstract(), Field.Store.YES));
-                document.add(new TextField("content", parser.getContent(), Field.Store.NO));
-
-                indexWriter.addDocument(document);
-                indexedFiles++;
-
-                printLoadingBar(indexedFiles, totalFiles);
-            }
-        }
-
-        long endTime = System.currentTimeMillis();
-        long elapsedTime = endTime - startTime;
-
-        log.info("Indexing completed. Files: {}. Elapsed time: {} seconds", indexedFiles, elapsedTime / 1000);
-
-        indexWriter.commit();
-        indexWriter.close();
-    }
-
-    public List<DocumentDto> search(String queryString) throws IOException, ParseException {
         List<DocumentDto> documents = new ArrayList<>();
 
         Query query = queryParser.parse(queryString);
@@ -130,7 +51,10 @@ public class SearchService {
 
         }
 
-        return documents;
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        return new SearchDto(documents, "", elapsedTime);
     }
 
 }
