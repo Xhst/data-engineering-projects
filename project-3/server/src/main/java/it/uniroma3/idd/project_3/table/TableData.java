bc45c376194483647135d30660cdbeab1d058f22
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-17 20:24:34 +0100

    feat: table data

---
+++ project-3/server/src/main/java/it/uniroma3/idd/project_3/table/TableData.java
@@ -0,0 +1,11 @@
+package it.uniroma3.idd.project_3.table;
+
+public record TableData(
+        String paperId,
+        String tableId,
+        String caption,
+        String htmlTable,
+        String footnotes,
+        String references
+) {
+}
