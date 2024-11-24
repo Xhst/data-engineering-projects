8814e28961f5772d71ebdd9e44f27f31765d75e4
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-24 16:30:58 +0100

    feat: table search dto

---
+++ project-3/table-searcher/dto.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class TableDto(BaseModel):
+    paperId: str
+    tableId: str
+    score: float
+    
+
+class TableSearchDto(BaseModel):
+    tables: list[TableDto]
+    suggestion: str
+    queryTimeMs: int
