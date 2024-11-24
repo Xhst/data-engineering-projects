5d8946eed0309d1101ec2927e619077e9865e01a
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-24 16:33:23 +0100

    feat: evaluation

--- project-3/evaluation/evaluation.py
+++ project-3/evaluation/evaluation.py
@@ -1,36 +1,50 @@
-import sys
-import os
 import json
-from collections import defaultdict
+import requests
 
-# Add the desired path to sys.path
-sys.path.append('../table-searcher')
+models = ["bert-base-uncased", "distilbert-base-uncased", "allenai/scibert_scivocab_uncased"]
+functions = ["tab_embedding", "tab_cap_embedding", "tab_cap_ref_embedding", "weighted_embedding"]
+queries = [
+    "NDCG on movielens dataset",
+    "Recommender systems Recall on dataset goodbook",
+    "Recommender systems MRR",
+    "Deep Learning dataset Apple Flower",
+    "Deep Learning GPT-3 precision and f1"
+    "Deep Learning GPT-3 precision and f-measure"
+]
 
-# Now you can import the module
-from paths import TABLE_FOLDER, GROUND_TRUTH, RESULTS
+results_json = {}
 
+results_json["lucene"] = {}
+for i, query in enumerate(queries, start=1):
+    response = requests.get(f"http://localhost:3000/api/search/tables?query={query}&model_name=lucene&method_name=lucene&use_hybrid=false&use_ground_truth=true")
+    query_key = f"q{i}"
+    results_json["lucene"][query_key] = {}
 
-def evaluate_system(path_to_system: str) -> tuple[float, float]:
-    mrr, ndcg = [0, 0]
-    
-    search_results: list[dict[int, dict[str,str,float]]] = [defaultdict(dict)]
-    
-    for file_name in os.listdir(path_to_system):
-        # Build the full file path
-        file_path = os.path.join(path_to_system, file_name)
-        
-        # Check if it's a JSON file
-        if os.path.isfile(file_path) and file_name.endswith(".json"):
-            # Open and load the JSON file
-            with open(file_path, 'r') as json_file:
-                try:
-                    query_data: dict[int, dict[str,str,float]] = json.load(json_file)
-                    search_results.append(query_data)
-                except json.JSONDecodeError as e:
-                    print(f"Error decoding JSON in file {file_name}: {e}")
+    pos = 1
+    for table in response.json()['tables']:
+        results_json["lucene"][query_key][str(pos)] = table.get('paperId') + "#" + table.get('tableId')
+        pos += 1
 
-        # Print the list of JSON data
-        print(search_results)
-    
-    
-    return mrr, ndcg
+for model in models:
+    results_json[model] = {}
+    for function in functions:
+        results_json[model][function] = {}
+
+        for i, query in enumerate(queries, start=1):
+
+            response = requests.get(f"http://localhost:3000/api/search/tables?query={query}&model_name={model}&method_name={function}&use_hybrid=false&use_ground_truth=true")
+            query_key = f"q{i}"
+            results_json[model][function][query_key] = {}
+
+            pos = 1
+            for table in response.json()['tables']:
+                results_json[model][function][query_key][str(pos)] = table.get('paperId') + "#" + table.get('tableId')
+                pos += 1
+
+            print(f"Results for query {query_key} with model {model} and function {function} done.")
+
+output_file = "./results.json"
+with open(output_file, "w") as f:
+    json.dump(results_json, f, indent=4)
+
+print(f"Results saved to {output_file}")
