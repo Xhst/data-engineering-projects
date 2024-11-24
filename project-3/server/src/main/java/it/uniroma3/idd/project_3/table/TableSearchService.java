529fa1287684ec06d0c2d5346b4d27ee226941c1
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-24 16:33:15 +0100

    chore: table search service

--- project-3/server/src/main/java/it/uniroma3/idd/project_3/table/TableSearchService.java
+++ project-3/server/src/main/java/it/uniroma3/idd/project_3/table/TableSearchService.java
@@ -1,5 +1,6 @@
 package it.uniroma3.idd.project_3.table;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.StoredFields;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
@@ -17,12 +18,19 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class TableSearchService {
 
     private final IndexSearcher indexSearcher;
     private final MultiFieldQueryParser queryParser;
     private final RestTemplate restTemplate;
+
+    private final List<String> groundTruthPapers = List.of(
+            "2008.03797", "2102.08921", "2301.04366v1", "2404.17723", "2405.02156v1", "2405.17060v1",
+            "2406.02638v2", "2407.03440", "2407.09157v1", "2407.13531v1", "2408.04641v1", "2408.09646v1",
+            "2409.10272", "2409.10309v2", "2409.17165v1", "2409.17400"
+    );
 
     public TableSearchService(@Qualifier("tableIndexSearcher") IndexSearcher indexSearcher,
                               @Qualifier("tableQueryParser") MultiFieldQueryParser queryParser,
@@ -33,44 +41,48 @@ public class TableSearchService {
     }
 
 
-    public TableSearchDto search(String queryString, int numberOfResults) throws IOException, ParseException {
+    public TableSearchDto search(String queryString, String modelName, String methodName, int numberOfResults,
+                                 boolean useHybridApproach, boolean useGroundTruth) throws IOException, ParseException {
         long startTime = System.currentTimeMillis();
         List<TableDto> tables = new ArrayList<>();
+        List<String> paperIds = new ArrayList<>();
 
-        List<String> paperIds = List.of("a", "b", "c");
+        if (!useHybridApproach) {
+            Query query = queryParser.parse(queryString);
 
-        // Construct the query string manually
-        String paperIdsQuery = String.join(",", paperIds);
+            TopDocs topDocs = indexSearcher.search(query, numberOfResults);
+
+            StoredFields storedFields = indexSearcher.storedFields();
+            for (int i = 0; i < topDocs.scoreDocs.length; i++) {
+                ScoreDoc scoreDoc = topDocs.scoreDocs[i];
+                Document doc = storedFields.document(scoreDoc.doc);
+
+                if (useGroundTruth && !groundTruthPapers.contains(doc.get("paper_id"))) continue;
+
+                paperIds.add(doc.get("paper_id"));
+
+                tables.add(new TableDto(
+                        doc.get("paper_id"),
+                        doc.get("table_id"),
+                        scoreDoc.score
+                ));
+            }
+
+            long endTime = System.currentTimeMillis();
+            long elapsedTime = endTime - startTime;
+
+            if (methodName.equals("lucene") || modelName.equals("lucene")) {
+                return new TableSearchDto(tables, "", elapsedTime);
+            }
+        }
 
         ResponseEntity<TableSearchDto> response = restTemplate.getForEntity(
-                "http://127.0.0.1:8000/api/table/search?query={query}&paper_ids={paper_ids}",
+                "http://127.0.0.1:8000/api/table/search?query={query}&paper_ids={paper_ids}&model_name={model_name}&method_name={method_name}&use_hybrid_approach={use_hybrid_approach}",
                 TableSearchDto.class,
-                queryString, paperIdsQuery
+                queryString, paperIds, modelName, methodName, useHybridApproach
         );
 
         return response.getBody();
-        /*
-        Query query = queryParser.parse(queryString);
-
-        TopDocs topDocs = indexSearcher.search(query, numberOfResults);
-
-        StoredFields storedFields = indexSearcher.storedFields();
-        for (int i = 0; i < topDocs.scoreDocs.length; i++) {
-            ScoreDoc scoreDoc = topDocs.scoreDocs[i];
-            Document doc = storedFields.document(scoreDoc.doc);
-
-            tables.add(new TableDto(
-                    doc.get("paper_id"),
-                    doc.get("table_id"),
-                    scoreDoc.score
-            ));
-
-        }
-
-        long endTime = System.currentTimeMillis();
-        long elapsedTime = endTime - startTime;
-
-        return new TableSearchDto(tables, "", elapsedTime);*/
     }
 
 }
