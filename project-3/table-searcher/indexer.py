22ae13e3dd16e388d047b1045485703e0122e465
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-24 16:31:31 +0100

    feat: indexer

--- project-3/table-searcher/indexer.py
+++ project-3/table-searcher/indexer.py
@@ -2,28 +2,56 @@ import pymilvus as pm
 import json
 import paths
 import os
+import time
+from table_embedding import get_function_from_name
 from index_schema import schema
 from embedder import Embedder
 
-embedder = Embedder()
 
-def convert_json_to_entities(data, paper_id):
+def convert_json_to_entities(data, paper_id, embedder: Embedder, function):
     entities = []
     for table_id, table_data in data.items():
         entities.append({
             "id": f"{paper_id}#{table_id}",
             "paper_id": paper_id,
             "table_id": table_id,
-            "vector": embedder.get_sentence_embedding(
-                table_data["table"] 
-                + table_data["caption"] 
-                + " ".join(table_data["references"])
-            )
+            "vector": function(embedder, table_data),
         })
     return entities
+    
+
+def create_index(embedder: Embedder, function_name: str):
+    function = get_function_from_name(function_name)
+
+    collection_name = ("table_" + embedder.model_name + "_" + function_name).replace("/", "_").replace("-", "_")
+
+    if pm.has_collection(collection_name):
+        pm.Collection(collection_name).drop()
+
+    collection = pm.Collection(name=collection_name, schema=schema)
+
+    for filename in os.listdir(paths.TABLE_FOLDER):
+        if filename.endswith(".json"):
+            with open(os.path.join(paths.TABLE_FOLDER, filename), "r", encoding="utf-8") as file:
+                paper_id = filename.replace(".json", "")
+                try:
+                    data = json.load(file)
+                    collection.insert(convert_json_to_entities(data, paper_id, embedder, function))
+                except Exception as e:
+                    continue
+    
+    # IVF_FLAT -> Inverted File with Flat Index
+    index_params = {"index_type": "IVF_FLAT", "metric_type": "COSINE", "params": {"nlist": 200}}
+
+    collection.create_index(field_name="vector", index_params=index_params)
+
+    collection.load()
 
 
 if __name__ == "__main__":
+    models = ["bert-base-uncased", "distilbert-base-uncased", "allenai/scibert_scivocab_uncased"]
+    functions = ["tab_embedding", "tab_cap_embedding", "tab_cap_ref_embedding", "weighted_embedding"]
+
     try:
         pm.connections.connect("default", host="localhost", port="19530")
         print("Connection successful!")
@@ -31,18 +59,12 @@ if __name__ == "__main__":
         print(f"Failed to connect to Milvus: {e}")
         exit()
 
-    collection_name = "table_collection"
-
-    if pm.has_collection(collection_name):
-        pm.Collection(collection_name).drop()
-
-    collection = pm.Collection(name=collection_name, schema=schema)
-
-    for filename in os.listdir(paths.GROUND_TRUTH + "/sample"):
-        if filename.endswith(".json"):
-            print(f"Indexing {filename}")
-            with open(os.path.join(paths.GROUND_TRUTH + "/sample", filename), "r", encoding="utf-8") as file:
-                paper_id = filename.replace(".json", "")
-                data = json.load(file)
-                collection.insert(convert_json_to_entities(data, paper_id))
+    for model in models:
+        for function in functions:
+            print(f"Creating index with model {model} and function {function}...")
+            start_time = time.time()
+            embedder = Embedder(model_name=model)
+            create_index(embedder, function_name=function)
+            elapsed_time = time.time() - start_time
+            print(f"Indexing done in {elapsed_time:.2f} seconds.")
         
