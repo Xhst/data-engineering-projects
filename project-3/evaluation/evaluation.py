a5eb3bb11157c1403b907cbf9346e09e7496a8bf
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-25 15:20:43 +0100

    chore: evaluation get update

--- project-3/evaluation/evaluation.py
+++ project-3/evaluation/evaluation.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-models = ["bert-base-uncased", "distilbert-base-uncased", "allenai/scibert_scivocab_uncased"]
+models = ["allenai/scibert_scivocab_uncased", "bert-base-uncased", "distilbert-base-uncased"]
 functions = ["tab_embedding", "tab_cap_embedding", "tab_cap_ref_embedding", "weighted_embedding"]
 queries = [
     "NDCG on movielens dataset",
@@ -16,7 +16,7 @@ results_json = {}
 
 results_json["lucene"] = {}
 for i, query in enumerate(queries, start=1):
-    response = requests.get(f"http://localhost:3000/api/search/tables?query={query}&model_name=lucene&method_name=lucene&use_hybrid=false&use_ground_truth=true")
+    response = requests.get(f"http://localhost:3000/api/search/tables?query={query}&modelName=lucene&methodName=lucene&useHybrid=false&useGroundTruth=true")
     query_key = f"q{i}"
     results_json["lucene"][query_key] = {}
 
@@ -31,8 +31,7 @@ for model in models:
         results_json[model][function] = {}
 
         for i, query in enumerate(queries, start=1):
-
-            response = requests.get(f"http://localhost:3000/api/search/tables?query={query}&model_name={model}&method_name={function}&use_hybrid=false&use_ground_truth=true")
+            response = requests.get(f"http://localhost:3000/api/search/tables?query={query}&modelName={model}&methodName={function}&useHybrid=false&useGroundTruth=true")
             query_key = f"q{i}"
             results_json[model][function][query_key] = {}
 
