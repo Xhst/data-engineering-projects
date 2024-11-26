7f3efa77d6c147803282c6abeb3b4ea6fd9a863e
Xhst <82611659+Xhst@users.noreply.github.com>
2024-11-26 09:12:54 +0100

    chore: sentence transformer

--- project-3/table-searcher/embedder.py
+++ project-3/table-searcher/embedder.py
@@ -1,30 +1,18 @@
 import torch
 import numpy as np
-from transformers import AutoTokenizer, AutoModel
+from sentence_transformers import SentenceTransformer
 
 
 class Embedder:
     def __init__(self, model_name: str = "bert-base-uncased"):
         self.model_name = model_name
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        self.model = AutoModel.from_pretrained(self.model_name)
+        self.model = SentenceTransformer(self.model_name)
         self.device = self.assign_device("cuda")
         self.model = self.model.to(self.device)
 
 
     def get_sentence_embedding(self, sentence: str) -> np.ndarray:
-        tokens = self.tokenizer(sentence, return_tensors="pt", padding=True, truncation=True, max_length=512)
-        tokens = {key: val.to(self.device) for key, val in tokens.items()}
-        
-        with torch.no_grad():
-            outputs = self.model(**tokens)
-        
-        embeddings = outputs.last_hidden_state
-        mask = tokens['attention_mask'].unsqueeze(-1)
-        masked_embeddings = embeddings * mask
-        sentence_embedding = masked_embeddings.sum(dim=1) / mask.sum(dim=1)
-        
-        return sentence_embedding.squeeze().cpu().numpy()
+        return self.model.encode(sentence)
     
 
     def assign_device(self, device_name: str):
