21422df8d0043314503330e0f3591ba736c8ba55
Xhst <82611659+Xhst@users.noreply.github.com>
2024-12-24 19:09:22 +0100

    chore(project-5): changed embedder model

--- project-5/src/embedder.py
+++ project-5/src/embedder.py
@@ -4,7 +4,7 @@ from sentence_transformers import SentenceTransformer
 
 
 class Embedder:
-    def __init__(self, model_name: str = "bert-base-uncased"):
+    def __init__(self, model_name: str = "paraphrase-MPNet-base-v2"):
         self.model_name = model_name
         self.model = SentenceTransformer(self.model_name)
         self.device = self.assign_device("cpu")
