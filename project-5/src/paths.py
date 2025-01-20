a012286b57f21f2c80848e78db331bfb064b4a85
Alessandro Di Girolamo <alessandro.digirolamo.1937@gmail.com>
2025-01-20 16:27:13 +0100

    ft(project5): file for paths

---
+++ project-5/src/paths.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class BLOCKING(Enum): 
+    RESULTS = '../blocking/results/'
+
+class PAIRWISE_MATCHING(Enum):  
+    RESULTS_DM = '../pairwise_matching/results/deep_matcher'
