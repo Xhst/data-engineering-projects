bde0406fc4844eb85a5003f28a1b1b819703491f
Alessandro Di Girolamo <alessandro.digirolamo.1937@gmail.com>
2025-01-20 19:17:45 +0100

    ft(project5): pairwise matching evaluation

---
+++ project-5/src/pairwise_matching/pm_evaluation.py
@@ -0,0 +1,76 @@
+import json
+import sys
+import os
+import re
+
+### --------- ###
+prv_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(prv_folder)
+import paths
+from ansi_colors import *
+### --------- ###
+
+# Pairs can be (str1, str2) in GT and (str2, str1) in json file... need normalization
+def normalize_pair(pair):
+    return tuple(sorted(pair))
+
+def evaluate(gt_file_path, json_file_path):
+    with open(gt_file_path, 'r', encoding='utf-8') as gt_file:
+        gt_lines = gt_file.readlines()
+
+    with open(json_file_path, 'r', encoding='utf-8') as json_file:
+        json_data = json.load(json_file)
+
+
+    gt_pairs = set()
+    gt_name_set = set()
+
+    for line in gt_lines:
+        line = line.strip()
+        if not line:
+            continue
+
+        pair = tuple(re.findall(r'"([^"]*)"', line))
+
+        str1, str2 = pair
+        gt_name_set.add(str1)
+        gt_name_set.add(str2)
+
+        gt_pairs.add((pair)) 
+
+
+    predicted_pairs = set()
+
+    for entry in json_data:
+        pair = (entry['entry1'], entry['entry2'])
+
+        str1, str2 = pair
+        if str1 in gt_name_set and str2 in gt_name_set:
+            predicted_pairs.add(pair)
+
+    gt_pairs_normalized = {normalize_pair(pair) for pair in gt_pairs}
+    predicted_pairs_normalized = {normalize_pair(pair) for pair in predicted_pairs}
+
+    correct_pairs = correct_pairs = gt_pairs_normalized.intersection(predicted_pairs_normalized)
+
+
+    precision = len(correct_pairs) / len(predicted_pairs_normalized) if predicted_pairs_normalized else 0
+    recall = len(correct_pairs) / len(gt_pairs_normalized) if gt_pairs_normalized else 0
+    f1 = 2 * ((precision * recall) / (precision + recall)) if (precision + recall) > 0 else 0
+
+    print(f"\n{CYAN}Pairwise matching EVALUATION for {json_file_path.rsplit('/', 1)[-1]}{RESET}")
+    print(f"- {GREEN}Precision: {RESET}{precision}")
+    print(f"- {GREEN}Precision: {RESET}{recall}")
+    print(f"- {GREEN}F1: {RESET}{f1}\n")
+    
+
+
+
+
+if __name__ == "__main__":
+
+    gt_file_path = f"{paths.GROUND_TRUTH}/gt.txt"
+    json_file_path = f"{paths.PAIRWISE_MATCHING.RESULTS_JAROWINKLER.value}/lsh_words.json"
+
+    evaluate(gt_file_path, json_file_path)
+
