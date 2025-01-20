01f2b0fe005775af46e66eb3f79d2433109f1155
Alessandro Di Girolamo <alessandro.digirolamo.1937@gmail.com>
2025-01-20 19:17:22 +0100

    ch(project5): new paths

--- project-5/src/paths.py
+++ project-5/src/paths.py
@@ -1,7 +1,10 @@
 from enum import Enum
 
+GROUND_TRUTH = '../ground_truth'
+
 class BLOCKING(Enum): 
     RESULTS = '../blocking/results/'
 
 class PAIRWISE_MATCHING(Enum):  
     RESULTS_DM = '../pairwise_matching/results/deep_matcher'
+    RESULTS_JAROWINKLER = '../pairwise_matching/results/jarowinkler'
