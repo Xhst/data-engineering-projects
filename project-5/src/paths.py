9c149abf94f1213cfbdb4f446ab11f9e84019f90
Alessandro Di Girolamo <alessandro.digirolamo.1937@gmail.com>
2025-01-22 16:58:39 +0100

    ch(project5): new paths

--- project-5/src/paths.py
+++ project-5/src/paths.py
@@ -1,6 +1,11 @@
 from enum import Enum
 
 GROUND_TRUTH = '../ground_truth'
+DATASET = '../dataset'
+
+class MODELS(Enum):
+    DATA_SPLITS = '../models/data_splits'
+    DEEP_MATCHER = '../models/deep_matcher'
 
 class BLOCKING(Enum): 
     RESULTS = '../blocking/results/'
