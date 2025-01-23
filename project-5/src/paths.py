b621bca071c9ca9d404b12f5dce5ce11e7397ccb
Alessandro Di Girolamo <alessandro.digirolamo.1937@gmail.com>
2025-01-23 17:44:04 +0100

    ch(project5): paths for deep matcher model and auxiliary folders for DM model predictions

--- project-5/src/paths.py
+++ project-5/src/paths.py
@@ -5,11 +5,13 @@ DATASET = '../dataset'
 
 class MODELS(Enum):
     DATA_SPLITS = '../models/data_splits'
-    DEEP_MATCHER = '../models/deep_matcher'
+    DEEP_MATCHER = '../models/deep_matcher/pw_matching_DM_model_lr0.0001_bs_16_epochs_10.pth'
 
 class BLOCKING(Enum): 
     RESULTS = '../blocking/results/'
 
 class PAIRWISE_MATCHING(Enum):  
     RESULTS_DM = '../pairwise_matching/results/deep_matcher'
+    RESULTS_DM_RAW = '../pairwise_matching/results/deep_matcher/unlabeled_pairs'
+    RESULTS_DM_PREDICTED = '../pairwise_matching/results/deep_matcher/pairs_prediction'
     RESULTS_JAROWINKLER = '../pairwise_matching/results/jarowinkler'
